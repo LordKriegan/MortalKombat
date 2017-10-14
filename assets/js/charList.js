8bfe02a7df4ece5cd6e78d83ee699e31f1ee28fd
Qamar Stationwala <YoPappy0@gmail.com>
2017-10-14 07:34:39 -0700

    borrowed stats from another persons starwars game

--- assets/js/charList.js
+++ assets/js/charList.js
@@ -1,34 +1,42 @@
+//borrowed this guys' numbers
+//https://github.com/cbrenner04/week-4-game/blob/master/assets/javascript/objects.js
+//liu kang = luke skywalker
+//raiden = emperor palpatine
+//sonya blade = darth vader
+//kitana = obi-wan kenobi
+//winning path = kitana, liu kang, raiden, sonya blade excluding your character.
+//losing paths are (apparently) mostly in opposite order
 var charList = [
 	{
 		name: "Liu Kang",
 		sex: "male",
-		atkPower: 11,
-		ctrAtkPower: 12,
-		health: 95,
+		atkPower: 40,
+		ctrAtkPower: 20,
+		health: 125,
 		img: "assets/images/characters/liukang.png"
 	},
 	{
 		name: "Raiden",
 		sex: "male",
-		atkPower: 12,
-		ctrAtkPower: 11,
-		health: 130,
+		atkPower: 60,
+		ctrAtkPower: 30,
+		health: 100,
 		img: "assets/images/characters/raiden.png"
 	},
 	{
 		name: "Sonya Blade",
 		sex: "female",
-		atkPower: 10,
-		ctrAtkPower: 11,
-		health: 110,
+		atkPower: 80,
+		ctrAtkPower: 40,
+		health: 80,
 		img: "assets/images/characters/sonyablade.png"
 	},
 	{
 		name: "Kitana",
 		sex: "female",
-		atkPower: 9,
+		atkPower: 20,
 		ctrAtkPower: 10,
-		health: 115,
+		health: 200,
 		img: "assets/images/characters/kitana.png"
 	}
 ]
